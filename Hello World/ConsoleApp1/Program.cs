d2c76ec2104aee3601403270540b1fb7dfdcb9b6
jo2916 <80034779+jo2916@users.noreply.github.com>
2021-03-11 12:07:29 +0100

    Update Program.cs
    Das war mist niklas!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Hello World/ConsoleApp1/Program.cs
+++ Hello World/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hjgdetvcgfsxngxdfgfxhx");
+            Console.WriteLine("Das war blödss");
             Console.ReadKey();
         }
     }
