369f898103af7ff10ccc4ce46459242f929ec741
jo2916 <80034779+jo2916@users.noreply.github.com>
2021-03-11 11:57:52 +0100

    Create Hello World von Jonas.cs
    ich habe n chance gemacht

---
+++ Hello World von Jonas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            Console.WriteLine("Hello World von XXX");
+            Console.WriteLine("Nǐ hǎo, shìjiè");
+            Console.WriteLine("Hallo Welt von XXX");
+            Console.WriteLine("marhabaan bialealam");
+            Console.ReadKey();
+
+        }
+    }
+}
