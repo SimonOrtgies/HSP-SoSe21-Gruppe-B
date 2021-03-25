6683df301fa81a3d40f5faffdda5d9b8c0f5b340
SimonOrtgies <simon.ortgies@student.jade-hs.de>
2021-03-25 17:11:42 +0100

    Kein Crocodile mehr

--- Workflow Test/Workflow Test/Program.cs
+++ Workflow Test/Workflow Test/Program.cs
@@ -14,7 +14,7 @@ namespace Hello_World_Mecke
         static void Main(string[] args) // Hier beginnt das Hauptprogramm 
         {
             // Ausgabe Benutzer 1
-            Console.WriteLine("Goodbye Crocodile");
+            Console.WriteLine("N´abend");
 
             // Ausgabe Benutzer 2
             Console.WriteLine("Nǐ hǎo, shìjiè");
