09e250427fc9380edae17244eada4c6b96d22924
jo2916 <80034779+jo2916@users.noreply.github.com>
2021-03-25 16:37:11 +0100

    Update Program.cs
    Ich habe Zeile 17 bearbeitet

--- Workflow Test/Workflow Test/Program.cs
+++ Workflow Test/Workflow Test/Program.cs
@@ -14,7 +14,7 @@ namespace Hello_World_Mecke
         static void Main(string[] args) // Hier beginnt das Hauptprogramm 
         {
             // Ausgabe Benutzer 1
-            Console.WriteLine("Hello World von YYY");
+            Console.WriteLine("See you Later alligator");
 
             // Ausgabe Benutzer 2
             Console.WriteLine("Nǐ hǎo, shìjiè");
