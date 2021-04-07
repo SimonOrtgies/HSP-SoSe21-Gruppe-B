76edaf249dcd7b9c24dfcd5401b22659f1b04bfe
SimonOrtgies <simon.ortgies@student.jade-hs.de>
2021-04-07 11:40:53 +0200

    Neues Projekt

---
+++ Schraubentechnik GmbH und Co. KG/Schraubentechnik GmbH und Co. KG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Schraubentechnik_GmbH_und_Co._KG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
