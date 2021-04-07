85663e0077152256552e6df36f3050ba0c713918
jo2916 <80034779+jo2916@users.noreply.github.com>
2021-04-07 12:49:07 +0200

    Update Program.cs

--- Schraubentechnik GmbH und Co. KG/Schraubentechnik GmbH und Co. KG/Program.cs
+++ Schraubentechnik GmbH und Co. KG/Schraubentechnik GmbH und Co. KG/Program.cs
@@ -6,7 +6,52 @@ namespace Schraubentechnik_GmbH_und_Co._KG
     {
         static void Main(string[] args)
         {
+            Boolean Rechtsgewinde = true;
+            string i;
+            int j = 1;
+            
+            
             Console.WriteLine("Willkommen beim Schraubenprogramm");
+
+            // Abfrage Rechtsgewinde - Linksgewinde
+
+            while (j == 1 )
+            {
+                Console.WriteLine("Rechtsgewinde(r) oder Linksgewinde(l)");
+                i = Console.ReadLine();
+                if(i == "r")
+                {
+                    Rechtsgewinde = true;
+                    j = 0;
+                }
+                else if(i == "l")
+                {
+                    Rechtsgewinde = false;
+                    j = 0;
+                }
+                else 
+                {
+                    Console.WriteLine("Ungültige Eingabe!");
+                }
+            }
+                
+                
+
+            //KONTROLLE
+            if(Rechtsgewinde == true)
+            {
+                Console.WriteLine("Rechtsgewinde");
+            }
+            else
+            {
+                Console.WriteLine("Linksgewinde");
+            }
+
+            //Eingabe Gewindegröße
+
+            Console.WriteLine("Geben Sie die Gewindegröße ein");
+
+
         }
     }
 }
