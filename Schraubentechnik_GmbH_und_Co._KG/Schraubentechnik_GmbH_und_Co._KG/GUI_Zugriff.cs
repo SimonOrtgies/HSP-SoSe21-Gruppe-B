a323e679990b825e8b3a2671073124514a99e9a5
ma6188 <mathis.plenge@student.jade-hs.de>
2021-04-29 18:00:58 +0200

    Grafikoberfläche eingefügt

---
+++ Schraubentechnik_GmbH_und_Co._KG/Schraubentechnik_GmbH_und_Co._KG/GUI_Zugriff.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Schraubentechnik_GmbH_und_Co._KG
+{
+    class GUI_Zugriff
+    {
+
+        GUI_Zugriff()
+        {
+            Window fenster = new Window();
+            GUI_Zugriff meineGUI = new GUI_Zugriff();
+            fenster.Title = "Schraubenprofis";
+            fenster.SizeToContent = SizeToContent.WidthAndHeight;
+            fenster.ResizeMode = ResizeMode.NoResize;
+            fenster.Content = meineGUI;
+            fenster.ShowDialog();
+
+        }
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new GUI_Zugriff();
+        }
+        
+    }
+}
---
+++ Schraubentechnik_GmbH_und_Co._KG/Schraubentechnik_GmbH_und_Co._KG/Grafikoberfläche.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Schraubentechnik_GmbH_und_Co._KG
+{
+    /// <summary>
+    /// Interaktionslogik für Grafikoberfläche.xaml
+    /// </summary>
+    public partial class Grafikoberfläche : UserControl
+    {
+        public Grafikoberfläche()
+        {
+            InitializeComponent();
+        }
+    }
+}
