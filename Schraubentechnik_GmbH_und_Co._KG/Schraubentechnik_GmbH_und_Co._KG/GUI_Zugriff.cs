0b900f1e28797ee3a4d56abf81eed2149bb08cae
niklas-bt <niklas.buthe@student.jade-hs.de>
2021-04-29 18:05:54 +0200

    Update GUI_Zugriff.cs
    Erklärungen für den Anfang

--- Schraubentechnik_GmbH_und_Co._KG/Schraubentechnik_GmbH_und_Co._KG/GUI_Zugriff.cs
+++ Schraubentechnik_GmbH_und_Co._KG/Schraubentechnik_GmbH_und_Co._KG/GUI_Zugriff.cs
@@ -12,19 +12,21 @@ namespace Schraubentechnik_GmbH_und_Co._KG
 
         GUI_Zugriff()
         {
-            Window fenster = new Window();
-            GUI_Zugriff meineGUI = new GUI_Zugriff();
-            fenster.Title = "Schraubenprofis";
-            fenster.SizeToContent = SizeToContent.WidthAndHeight;
-            fenster.ResizeMode = ResizeMode.NoResize;
-            fenster.Content = meineGUI;
-            fenster.ShowDialog();
+            Window fenster = new Window();         //using System.Window von oben wird aufgerufen
+            GUI_Zugriff meineGUI = new GUI_Zugriff();     //neues graphische Oberfläche Objekt wird erzeugt
+            fenster.Title = "Schraubenprofis";        //Name des Fensters
+            fenster.SizeToContent = SizeToContent.WidthAndHeight; //Größe des Fensters soll auf die Größe des Inhalts skaliert werden
+            fenster.ResizeMode = ResizeMode.NoResize;   //gibt die Möglichkeit, dass man das Fenster von der Größe her verändern kann (NoResize verhindert folgende Erklärung)
+            fenster.Content = meineGUI;     //Inhalt des fensters soll durch meineGUI festgelegt werden
+            fenster.ShowDialog();       //ShowDialog sorgt dafür dass zuerst das Fenster ausgeführt werden muss
 
+            Console.WriteLine("Press any key...");
+            //Console.ReadKey();
         }
-        [STAThread]
+        [STAThread] //GUI-Komponenten müssen in einem Prozess laufen
         static void Main(string[] args)
         {
-            new GUI_Zugriff();
+            new GUI_Zugriff();  //neues Objekt
         }
         
     }
