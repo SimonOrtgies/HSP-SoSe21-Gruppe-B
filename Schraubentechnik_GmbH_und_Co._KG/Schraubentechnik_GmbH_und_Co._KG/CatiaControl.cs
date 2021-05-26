4f965660c8cc000f919074339f5fea258abf343e
niklas-bt <niklas.buthe@student.jade-hs.de>
2021-05-26 11:53:59 +0200

    aktiviert gewinderichtung

--- Schraubentechnik_GmbH_und_Co._KG/Schraubentechnik_GmbH_und_Co._KG/CatiaControl.cs
+++ Schraubentechnik_GmbH_und_Co._KG/Schraubentechnik_GmbH_und_Co._KG/CatiaControl.cs
@@ -49,8 +49,7 @@ namespace Schraubentechnik_GmbH_und_Co._KG
 
                     //ErzeugeGewindeFeature
 
-                   // cc.ErzeugeGewindeFeature(s.gewinderichtung,s.metrischeGewindegroesse.bezeichnung, s.gewindeLaenge.gewindeLaenge); //WAS IST HIER PASSIERT??
-
+                    cc.ErzeugeGewindeFeature(s.gewinderichtung,s.metrischeGewindegroesse.bezeichnung, s.gewindeLaenge.gewindeLaenge);
                     //cc.ErzeugeGewindeFeature(20, 75);         //Test mit d=20, gewindelänge=75
                     Console.WriteLine("6");
 
